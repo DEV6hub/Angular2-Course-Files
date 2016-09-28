57d08982bfb8a5baf39eeaef95ac952e581653b0
tpadley <tylerp@dev6.com>
2016-09-28 09:28:00 -0400

    converted fourth exercise to new format

--- herding-cats/src/app.module.ts
+++ herding-cats/src/app.module.ts
@@ -3,10 +3,11 @@ import {AppComponent} from "./app/app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {CatDetailComponent} from "./app/cat-detail.component";
 import {FormsModule} from "@angular/forms";
+import {CatYearsPipe} from "./app/cat-years.pipe";
 
 @NgModule({
 	imports: [BrowserModule, FormsModule],
-	declarations: [AppComponent, CatDetailComponent],
+	declarations: [AppComponent, CatDetailComponent, CatYearsPipe],
 	bootstrap: [AppComponent]
 })
 
