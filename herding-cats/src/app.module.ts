056950bfd8a8ceb9f881737c349c61e0faa8d104
tpadley <tylerp@dev6.com>
2016-09-28 09:13:32 -0400

    converted third exercise to new format

--- herding-cats/src/app.module.ts
+++ herding-cats/src/app.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from "@angular/core";
 import {AppComponent} from "./app/app.component";
 import {BrowserModule} from "@angular/platform-browser";
 import {CatDetailComponent} from "./app/cat-detail.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-	imports: [BrowserModule],
+	imports: [BrowserModule, FormsModule],
 	declarations: [AppComponent, CatDetailComponent],
 	bootstrap: [AppComponent]
 })
